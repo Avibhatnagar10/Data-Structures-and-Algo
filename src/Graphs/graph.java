ea7632ca3b9cc861024fcd643ad104cf72e65791
Avibhatnagar10 <avibhatnagar10@gmail.com>
2024-07-06 00:50:53 +0530

    updates

---
+++ src/Graphs/graph.java
@@ -0,0 +1,16 @@
+public  class graph {
+    public static void main(String[] args) {
+        adjacentArray graph = new adjacentArray(5);
+
+        graph.addEdge(0, 1);
+        graph.addEdge(0, 4);
+        graph.addEdge(1, 2);
+        graph.addEdge(1, 3);
+        graph.addEdge(1, 4);
+        graph.addEdge(2, 3);
+        graph.addEdge(3, 4);
+
+        graph.printGraph();
+    }
+}
+ 
